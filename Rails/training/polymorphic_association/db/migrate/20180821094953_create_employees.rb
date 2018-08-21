346d8f4bfa755f0137506623d3c4589c665983aa
Hammad <hammadbhat66@gmail.com>
2018-08-21 15:54:27 +0530

    polymorphic_association with doc

---
+++ Rails/training/polymorphic_association/db/migrate/20180821094953_create_employees.rb
@@ -0,0 +1,10 @@
+class CreateEmployees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
---
+++ Rails/training/polymorphic_association/db/migrate/20180821095055_create_pictures.rb
@@ -0,0 +1,10 @@
+class CreatePictures < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pictures do |t|
+      t.string :name
+      t.references :imageable, polymorphic: true,index: true
+
+      t.timestamps
+    end
+  end
+end
